3507be8a955114d6b2ecb21c63d48d12ce3e80df
Vegard Løkås <vegardlokas@hotmail.com>
2023-11-16 15:33:56 +0100

    more cards

--- src/components/Dish.js
+++ src/components/Dish.js
@@ -10,38 +10,49 @@ const Dish = () => {
     {
       image: Salad,
       title: "Greek Salad",
+      price: "$12.99",
       description:
         "The famous Greek salad of crispy lettuce, peppers, olives and our Chicago styled feta cheese, garnished with crunchy garlic, rosemary croutons.",
       order: "Order a delivery",
     },
     {
       image: Bruschetta1,
-      title: "Greek Salad",
+      title: "Bruschetta",
+      price: "$5.99",
       description:
-        "The famous Greek salad of crispy lettuce, peppers, olives and our Chicago styled feta cheese, garnished with crunchy garlic, rosemary croutons.",
+        "Our Bruschetta is made from grilled bread that has been smeared with garlic and seasoned with salt and olive oil; made perfect for an evening dinner.",
       order: "Order a delivery",
     },
     {
       image: Creme,
-      title: "Greek Salad",
+      title: "Lemon Dessert",
+      price: "$5.00",
       description:
-        "The famous Greek salad of crispy lettuce, peppers, olives and our Chicago styled feta cheese, garnished with crunchy garlic, rosemary croutons.",
+        "This comes straight from Grandma's recipe book. Every last ingredient has been sourced and is as authentic as can be imagined.",
       order: "Order a delivery",
     },
   ];
 
   const mappedItems = menuItems.map((dish) => {
     return (
-      <div className="Dish">
-        <img src={dish.image} alt={dish.title} />
-        <h4>{dish.title}</h4>
-        <p>{dish.description}</p>
-        <button>{dish.order}</button>
-      </div>
+      <>
+        <div className="card">
+          <div className="image-container">
+            <img src={dish.image} alt={dish.title} />
+          </div>
+          <div className="dish-card-text">
+            <h3>{dish.title}</h3>
+            <h3>{dish.price}</h3>
+            <p>{dish.description}</p>
+          </div>
+
+          <button>{dish.order}</button>
+        </div>
+      </>
     );
   });
 
-  return <div>{mappedItems}</div>;
+  return <div className="dish-cards">{mappedItems}</div>;
 };
 
 export default Dish;
